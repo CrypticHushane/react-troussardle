23839214dc76b61423f9b6c8d5906350f128a693
CrypticHushane <oshanewilliams99@outlook.com>
2022-04-26 03:09:53 -0500

    change request url to https

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosRequest = axios.create({
-    baseUrl: "http://api.football-data.org/v2/",
+    baseUrl: "https://api.football-data.org/v2/",
 });
 
-axiosRequest.defaults.baseURL = 'http://api.football-data.org/v2/';
+axiosRequest.defaults.baseURL = 'https://api.football-data.org/v2/';
 axiosRequest.defaults.headers.common['X-Auth-Token'] = 'f872c10352604a04acf98d845da453ff';
 
 export default axiosRequest;
